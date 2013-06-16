10493cc9899b4d48066463895bbd9ede31d04f7c
David Bahia <david.bahia@blueyonder.co.uk>
2013-06-16 18:24:18 +0100

    Initial commit of BDD Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Libapp::Application.config.secret_token = '8b3342a6adc50f5c8bbae3c4d414d000dc653f43a3f80d50bc813085f23c729baafe519b51c26ec8841972dd78238c3fd4854dff6efeee8c5d12550009d74839'
